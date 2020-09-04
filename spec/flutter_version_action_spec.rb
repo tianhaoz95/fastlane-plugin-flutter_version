d30befb0c38dd6dd3672170c5d33e0ee54d9e915
Tianhao Zhou <tianhaoz@umich.edu>
2020-09-04 15:05:54 -0700

    chore: some maintenance (#28)
    * chore: update rubocop configuration
    
    * chore: upgrade checkin CI and add linter
    
    * docs: add gem registry instructions
    
    * fix: dependency issue
    
    * chore: update rake config
    
    * chore: remove unused CI services
    
    * chore: use rake for testing
    
    * chore: add release drafter

--- spec/flutter_version_action_spec.rb
+++ spec/flutter_version_action_spec.rb
@@ -1,9 +1,10 @@
 describe Fastlane::Actions::FlutterVersionAction do
-  describe "#run" do
-    it "prints a message" do
-      expect(Fastlane::UI).to receive(:message).with("The flutter_version plugin is working!")
-
-      Fastlane::Actions::FlutterVersionAction.run(nil)
+  describe "test" do
+    it "prints Flutter version information" do
+      expect(Fastlane::UI).to receive(:message).with("The full version is: 1.0.6+15")
+      expect(Fastlane::UI).to receive(:message).with("The version name: 1.0.6")
+      expect(Fastlane::UI).to receive(:message).with("The version code: 15")
+      Fastlane::Actions::FlutterVersionAction.run(pubspec_location: "./example/pubspec.yaml")
     end
   end
 end
