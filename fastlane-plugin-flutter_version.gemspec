f80279bc812a4aa7f6bf96a4167deb29d543a90c
Tianhao Zhou <tianhaoz@umich.edu>
2020-09-04 22:53:14 -0700

    chore: add code scan (#33)
    * chore: add code scan
    
    * fix: downgrade rubocop

--- fastlane-plugin-flutter_version.gemspec
+++ fastlane-plugin-flutter_version.gemspec
@@ -1,32 +1,37 @@
+# frozen_string_literal: true
+
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "fastlane/plugin/flutter_version/version"
+require 'fastlane/plugin/flutter_version/version'
 
 Gem::Specification.new do |spec|
-  spec.name = "fastlane-plugin-flutter_version"
+  spec.name = 'fastlane-plugin-flutter_version'
   spec.version = Fastlane::FlutterVersion::VERSION
   spec.required_ruby_version = '>= 2.6'
-  spec.author = "tianhaoz95"
-  spec.email = "tianhaoz@umich.edu"
+  spec.author = 'tianhaoz95'
+  spec.email = 'tianhaoz@umich.edu'
 
-  spec.summary = "A plugin to retrieve versioning information for Flutter projects."
-  spec.homepage = "https://github.com/tianhaoz95/fastlane-plugin-flutter_version"
-  spec.license = "MIT"
+  spec.summary = 'A plugin to retrieve ' \
+    'versioning information for Flutter ' \
+    'projects.'
+  spec.homepage = 'https://github.com/tianhaoz95/' \
+    'fastlane-plugin-flutter_version'
+  spec.license = 'MIT'
 
-  spec.files = Dir["lib/**/*"] + %w(README.md LICENSE)
+  spec.files = Dir['lib/**/*'] + %w[README.md LICENSE]
   spec.test_files = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
   # Don't add a dependency to fastlane or fastlane_re
   # since this would cause a circular dependency
 
-  spec.add_development_dependency("bundler")
-  spec.add_development_dependency("fastlane", ">= 2.142.0")
-  spec.add_development_dependency("pry")
-  spec.add_development_dependency("rake")
-  spec.add_development_dependency("rspec")
-  spec.add_development_dependency("rspec_junit_formatter")
-  spec.add_development_dependency("rubocop", "0.90.0")
-  spec.add_development_dependency("rubocop-require_tools")
-  spec.add_development_dependency("simplecov")
+  spec.add_development_dependency('bundler')
+  spec.add_development_dependency('fastlane', '>= 2.142.0')
+  spec.add_development_dependency('pry')
+  spec.add_development_dependency('rake')
+  spec.add_development_dependency('rspec')
+  spec.add_development_dependency('rspec_junit_formatter')
+  spec.add_development_dependency('rubocop')
+  spec.add_development_dependency('rubocop-require_tools')
+  spec.add_development_dependency('simplecov')
 end
--- lib/fastlane/plugin/flutter_version/actions/flutter_version_action.rb
+++ lib/fastlane/plugin/flutter_version/actions/flutter_version_action.rb
@@ -1,42 +1,51 @@
-require "fastlane/action"
-require "yaml"
-require_relative "../helper/flutter_version_helper"
+# frozen_string_literal: true
+
+require 'fastlane/action'
+require 'yaml'
+require_relative '../helper/flutter_version_helper'
 
 module Fastlane
   module Actions
+    # The top-level plugin interface
     class FlutterVersionAction < Action
       def self.run(params)
         pubspec_location = params[:pubspec_location]
-        pubspec = YAML.load_file(pubspec_location)
-        version = pubspec["version"]
-        UI.message("The full version is: ".concat(version))
-        unless version.include?("+")
-          raise "Verson code indicator (+) not found in pubspec.yml"
+        begin
+          pubspec = YAML.load_file(pubspec_location)
+        # rubocop:disable Style/RescueStandardError
+        rescue
+          raise 'Read pubspec.yaml failed'
+        end
+        # rubocop:enable Style/RescueStandardError
+        version = pubspec['version']
+        UI.message('The full version is: '.dup.concat(version))
+        unless version.include?('+')
+          raise 'Verson code indicator (+) not found in pubspec.yml'
         end
 
-        version_sections = version.split("+")
+        version_sections = version.split('+')
         version_name = version_sections[0]
         version_code = version_sections[1]
-        UI.message("The version name: ".concat(version_name))
-        UI.message("The version code: ".concat(version_code))
-        return {
-                 "version_code" => version_code,
-                 "version_name" => version_name
-               }
+        UI.message('The version name: '.dup.concat(version_name))
+        UI.message('The version code: '.dup.concat(version_code))
+        {
+          'version_code' => version_code,
+          'version_name' => version_name
+        }
       end
 
       def self.description
-        "A plugin to retrieve versioning information for Flutter projects."
+        'A plugin to retrieve versioning information for Flutter projects.'
       end
 
       def self.authors
-        ["tianhaoz95"]
+        ['tianhaoz95']
       end
 
       def self.return_value
         [
-          ["VERSION_CODE", "The version code"],
-          ["VERSION_NAME", "The verison name"]
+          ['VERSION_CODE', 'The version code'],
+          ['VERSION_NAME', 'The verison name']
         ]
       end
 
@@ -51,18 +60,20 @@ module Fastlane
         [
           FastlaneCore::ConfigItem.new(
             key: :pubspec_location,
-            env_name: "PUBSPEC_LOCATION",
-            description: "The location of pubspec.yml",
+            env_name: 'PUBSPEC_LOCATION',
+            description: 'The location of pubspec.yml',
             optional: true,
             type: String,
-            default_value: "../pubspec.yaml"
+            default_value: '../pubspec.yaml'
           )
         ]
       end
 
-      def self.is_supported?(platform)
+      # rubocop:disable Naming/PredicateName
+      def self.is_supported?(_platform)
         true
       end
+      # rubocop:enable Naming/PredicateName
     end
   end
 end
